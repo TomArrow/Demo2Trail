ff3951ea90b4933a6bdd1d22976efbe67d314c3a
david <videoprofess@gmail.com>
2018-03-06 16:06:42 -0500

    Code cleanup, default to ignore loopstarts New strafetrails pk3 that ignores loopstarts

--- demo2trail/main.cpp
+++ demo2trail/main.cpp
@@ -12,24 +12,20 @@
 #include <fcntl.h>
 #endif
 
+#define LOOPSTARTS 0
+#define TRAIL_BUFSIZE 8192 //Write to file in 8kb chunks
+
 int main(int argc, char** argv)
 {
-	//printf( "Demo2Trail v" VERSION " loaded\n");
 	char *demoFileName = NULL;
 	char *trailFileName = NULL;
-
-	//COM_GetExtension
-
 
 	if (argc == 2) {//Input file specified, use same name for output file 
 		char *input = argv[1];
 
 		if (COM_CompareExtension(input, ".dm_26")) { //Possible drag/drop, check extension and strip
 			char temp[256]; //wtf - max path?
-
-			//printf("Extension found\n");
 			COM_StripExtension(input, temp, sizeof(temp));
-
 			demoFileName = va("%s", input);
 			trailFileName = va("%s.cfg", temp);
 		}
@@ -37,14 +33,12 @@ int main(int argc, char** argv)
 			demoFileName = va("%s.dm_26", input);
 			trailFileName = va("%s.cfg", input);
 		}
-		//printf("1Input file: %s Output file: %s\n", demoFileName, trailFileName);
-		//system("pause");
 	}
 	else if (argc == 3) { //Input and output file specified
 		char *input = argv[1];
 		char *output = argv[2];
 
-		if (COM_CompareExtension(input, ".dm_26")) { //Possible drag/drop, check extension and strip
+		if (COM_CompareExtension(input, ".dm_26")) {
 			demoFileName = va("%s", input);
 		}
 		else { //if no extension -fixme
@@ -60,13 +54,10 @@ int main(int argc, char** argv)
 		else {
 			trailFileName = va("%s.cfg", input);
 		}
-		//printf("2Input file: %s Output file: %s\n", demoFileName, trailFileName);
-		//system("pause");
 	}
 	else {
 		printf("No file specified.\n"
 			"Usage: \"%s\" filename outfile (optional)\n", argv[0]);
-		//system("pause");
 		return -1;
 	}
 
@@ -76,7 +67,6 @@ int main(int argc, char** argv)
 	FS_FOpenFileRead(demoFileName, &fp, qfalse );
 	if ( !fp ) {
 		printf( "File %s not found.\n", demoFileName);
-		//system( "pause" );
 		return -1;
 	}
 
@@ -86,11 +76,14 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	char buf[TRAIL_BUFSIZE] = { 0 };
 	qboolean demoFinished = qfalse;
-	int lastRaceTime = 0;
+	int lastStartTime = 0;
+	int lastRaceTimer = 0;
+	int raceTimer = 0;
 	vec3_t lastOrigin = {0};
-	char buf[8192] = {0};
 	qboolean dontReset;
+
 	while ( !demoFinished ) {
 		msg_t msg;
 		byte msgData[ MAX_MSGLEN ];
@@ -98,15 +91,11 @@ int main(int argc, char** argv)
 		demoFinished = CL_ReadDemoMessage( fp, &msg ) ? qfalse : qtrue;
 
 		if ( demoFinished ) {
-			//write rest of buffer?
-			//printf("stopping 1\n");
 			break;
 		}
 		try {
 			CL_ParseServerMessage( &msg );
-		} catch ( int ) {
-			// thrown code means it wasn't a fatal error, so we can still dump what we had
-			//printf("stopping 2\n");
+		} catch ( int ) { // thrown code means it wasn't a fatal error, so we can still dump what we had
 			break;
 		}
 
@@ -114,8 +103,7 @@ int main(int argc, char** argv)
 			continue;
 		}
 		int firstServerCommand = ctx->clc.lastExecutedServerCommand;
-		// process any new server commands
-		for ( ; ctx->clc.lastExecutedServerCommand < ctx->clc.serverCommandSequence; ctx->clc.lastExecutedServerCommand++ ) {
+		for ( ; ctx->clc.lastExecutedServerCommand < ctx->clc.serverCommandSequence; ctx->clc.lastExecutedServerCommand++ ) { // process any new server commands
 			char *command = ctx->clc.serverCommands[ ctx->clc.lastExecutedServerCommand & ( MAX_RELIABLE_COMMANDS - 1 ) ];
 			Cmd_TokenizeString( command );
 			char *cmd = Cmd_Argv( 0 );
@@ -123,62 +111,55 @@ int main(int argc, char** argv)
 				firstServerCommand = ctx->clc.lastExecutedServerCommand;
 			}
 		}
-		//How 2 trim needless shit at start/end(?) of demo:
 
-		//If tele bit was flipped, reset file?  Nvm, that will break courses with teleports.
-		//If we had a timer and now we dont, reset file.  (this fucks up after we finish course)
-		//If we had a timer and now we have a different timer, reset. (that fucks loopstarts?)
-		//If we have a timer now, and we didnt previously, reset file (This fucks up prejumps, but oh wel?), also ruins it if person ends timer.. then comes back..
+		//If we have a timer now, and we didnt previously, reset file (this removes resets)
+		//If we have a timer now, and its less than our previous timer, reset file (this removes loopstarts)
+		//If current origin is same as previous... don't write anything...(possibly fucks up timing, but never happens in practice)
 
-		//If current origin is same as previous... don't write anything...? 
-		//(this fucks the timing.. but really are there any courses where people pause - besides mountain? and if so it can be done without the pause?)
+		int startTime = ctx->cl.snap.ps.duelTime;
+				
+		if (startTime)
+			raceTimer = ctx->cl.snap.serverTime - startTime;
+		else
+			raceTimer = 0;
 
-		if (ctx->cl.snap.ps.duelTime && ctx->cl.snap.serverTime - ctx->cl.snap.ps.duelTime > 6000) { //Dont clear file if we have gone more than 6sec into run, yikes.
-			//printf("dontreset!\n");
+		if (raceTimer > 6000) //Dont clear file if we have gone more than 6sec into run, yikes.
 			dontReset = qtrue;
+
+		if (startTime && !dontReset) {
+#if LOOPSTARTS
+			if (!lastStartTime)
+#else
+			if (!lastStartTime || (raceTimer < lastRaceTimer))
+#endif
+				Q_strncpyz(buf, "", sizeof(buf));
 		}
 
-		if (!lastRaceTime && ctx->cl.snap.ps.duelTime && !dontReset) { //pathetic hack
-			//printf("Stopping race\n");
-			//also clear file.. but that crashes..?
-			Q_strncpyz(buf, "", sizeof(buf));
-		}
+		lastStartTime = startTime;
+		lastRaceTimer = raceTimer;
 
-		lastRaceTime = ctx->cl.snap.ps.duelTime;
-
-		if (ctx->cl.snap.ps.duelTime) { //Only write if they are in a race
+		if (startTime) { //Only write if they are in a race
 			if (!VectorCompare(ctx->cl.snap.ps.origin, lastOrigin)) //Dont write if they are standing still
 			{
 				char *tmpMsg = NULL;
 				tmpMsg = va("%i %i %i\n", (int)(ctx->cl.snap.ps.origin[0] + 0.5f), (int)(ctx->cl.snap.ps.origin[1] + 0.5f), (int)(ctx->cl.snap.ps.origin[2] + 0.5f));
-
-				VectorCopy(ctx->cl.snap.ps.origin, lastOrigin);
-		
 				if (strlen(buf) + strlen(tmpMsg) >= sizeof(buf)) { //Write to file
 					fwrite(buf, strlen(buf), 1, trailFile);
 					Q_strncpyz(buf, "", sizeof(buf)); //buf[0] = '\0'; ?
-					//printf("Resetting buf!\n");
 				}
-
-				//printf("startedRace? %i time? %i\n", startedRace, ctx->cl.snap.ps.duelTime);
-
 				Q_strcat(buf, sizeof(buf), tmpMsg);
-			}/*
-			else {
-				printf("Standing still! - (%i %i %i)\n", (int)(ctx->cl.snap.ps.origin[0] + 0.5f), (int)(ctx->cl.snap.ps.origin[1] + 0.5f), (int)(ctx->cl.snap.ps.origin[2] + 0.5f));
-			}
-			*/
-		}
 
+				VectorCopy(ctx->cl.snap.ps.origin, lastOrigin);
+			}
+		}
 	}
 
-	//Write remaining buf to file
-	fwrite(buf, strlen(buf), 1, trailFile);
+	fwrite(buf, strlen(buf), 1, trailFile); //Write remaining buf to file
 
 	FS_FCloseFile( fp );
 	fclose( trailFile );
 
-	printf("Completed\n"); //printing filenames here fucks up??? memory?
+	printf("Completed\n"); //Printing filenames here fucks up??? memory?
 
 	return 0;
 }
